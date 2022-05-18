8b116b4227e115d5b983133bebc9dc685d5683da
Quang Truong <votruong284@gmail.com>
2022-05-18 15:58:40 +0700

    Clean code

--- README.md
+++ README.md
@@ -10,17 +10,17 @@
 8) Võ Quang Trường
 9) Phan Quốc Toàn 
 ## Các tài khoản quản trị
-_Tài khoản admin: admin2_
-_Pass:123_
+_Tài khoản admin: admin_  
+_Pass:123_  
 
-_Tài khoản nhân viên quản trị: nhan_vien_quan_tri_
-_Pass:123_
-_Tài khoản nhân viên bán hàng: nhan_vien_ban_hang_
-_Pass:123_
+_Tài khoản nhân viên quản trị: nhan_vien_quan_tri_  
+_Pass:123_  
+_Tài khoản nhân viên bán hàng: nhan_vien_ban_hang_  
+_Pass:123_  
 ## Các tài khoản người dùng 
-_levietsanh123@gmail.com_
-_Pass: 123456789_
-_trinhvantrieu123@gmail.com_
-_Pass: 123456789_
-_lehoangduc123@gmail.com_
-_Pass: 123456789_
+_levietsanh123@gmail.com_  
+_Pass: 123456789_  
+_trinhvantrieu123@gmail.com_  
+_Pass: 123456789_  
+_lehoangduc123@gmail.com_  
+_Pass: 123456789_  
--- chuc_nang/dang_ky_dang_nhap/exec_dang_nhap.php
+++ chuc_nang/dang_ky_dang_nhap/exec_dang_nhap.php
@@ -2,46 +2,46 @@
 
 
 
-<?php 
+<?php
 ob_start();
-if(!isset($_SESSION)){
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
 }
-	// if(isset($_POST['submit'])){
-	// 	include('connect.php');
-	// 	$pass= $_POST['password'];
-	// 	$pass= md5($pass);
-	// 	$sl="select * from thanh_vien where email='".$_POST['email']."' and password='".$pass."'";
-	// 	$exec= mysqli_query($connect,$sl);
-	// 	$kt = mysqli_num_rows($exec);
-	// 	$dangnhap=mysqli_fetch_array($exec);
-	// 	if($kt>0){
-	// 		$_SESSION['login']= $dangnhap;
-	// 		echo "<script> swal({
-	// 				icon: 'success',
-	// 				title: 'Đăng nhập thành công',
-	// 				text: 'Hill top watch xin chào bạn',
-	// 				button:null
-  
-	// 			})
-	// 			setTimeout(function(){   location.href='index.php';
-    //           },4000); </script>";
-					
-	// 	}
-	// 	else{
-	// 		echo "<script> alert('Đăng nhập thất bại'); location.href='?menu=dang_nhap'; </script>";
-	// 	}
-	// }
-	// if(isset($_POST['cancel'])){
-	// 	echo "<script> location.href='javascript: history.back(-1);'; </script>";
-	// }
+// if(isset($_POST['submit'])){
+// 	include('connect.php');
+// 	$pass= $_POST['password'];
+// 	$pass= md5($pass);
+// 	$sl="select * from thanh_vien where email='".$_POST['email']."' and password='".$pass."'";
+// 	$exec= mysqli_query($connect,$sl);
+// 	$kt = mysqli_num_rows($exec);
+// 	$dangnhap=mysqli_fetch_array($exec);
+// 	if($kt>0){
+// 		$_SESSION['login']= $dangnhap;
+// 		echo "<script> swal({
+// 				icon: 'success',
+// 				title: 'Đăng nhập thành công',
+// 				text: 'Hill top watch xin chào bạn',
+// 				button:null
+
+// 			})
+// 			setTimeout(function(){   location.href='index.php';
+//           },4000); </script>";
+
+// 	}
+// 	else{
+// 		echo "<script> alert('Đăng nhập thất bại'); location.href='?menu=dang_nhap'; </script>";
+// 	}
+// }
+// if(isset($_POST['cancel'])){
+// 	echo "<script> location.href='javascript: history.back(-1);'; </script>";
+// }
 
 
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "do_an_web2";
+$dbname = "cuahangdongho";
 
 // Create connection
 $con = new mysqli($servername, $username, $password, $dbname);
@@ -53,14 +53,14 @@ if ($con->connect_error) {
 
 $error = false;
 
-    # code...
+# code...
 
 if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['password']) && !empty($_POST['password'])) {
     $result = mysqli_query($con, "Select `id`,`email`,`password`,`hoten`,`sdt`,`diachi`,`status` from `thanh_vien` WHERE (`email` ='" . $_POST['email'] . "' AND `password` = md5('" . $_POST['password'] . "'))");
     if (!$result) {
         $error = mysqli_error($con);
     } else {
-         $dangnhap = mysqli_fetch_assoc($result);
+        $dangnhap = mysqli_fetch_assoc($result);
     }
     mysqli_close($con);
     if ($error !== false || $result->num_rows == 0) {
@@ -69,18 +69,17 @@ if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['password'
             'message' => 'Thông tin đăng nhập không đúng'
         ));
         exit;
-    } else if($dangnhap['status']==1) {
-       
-        $_SESSION['login']= $dangnhap;
+    } else if ($dangnhap['status'] == 1) {
+
+        $_SESSION['login'] = $dangnhap;
         echo json_encode(array(
             'status' => $dangnhap['status'],
             'message' => 'Đăng nhập thành công'
         ));
-       
+
         exit;
-    }
-    else if ($dangnhap['status']==2) {
-           echo json_encode(array(
+    } else if ($dangnhap['status'] == 2) {
+        echo json_encode(array(
             'status' => 3,
             'message' => 'Xin lỗi tài khoản của bạn đang bị khóa'
         ));
